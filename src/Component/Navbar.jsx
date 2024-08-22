6b9c3e35bb8c16cb1387d3591bb800df5c587fd9
Eshun Calvin Williamson <kwekujur143@gmail.com>
2024-08-22 13:38:29 +0000

    all screen sizes done

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar =()=>{
                         <li><Link to='/about'>ABOUT</Link></li>
                          <li><Link to='/our'> OUR TEAM</Link></li>
                         <li><Link to='/contact'>CONTACT US</Link></li>
-                        <li><Link to="/join">JOIN US</Link></li>
+                        <li className="exist"><Link to="/join">JOIN US</Link></li>
                     </ul>
                 </nav>
             </div>  
