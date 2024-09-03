aba91bdab5234163eeee83ddb361d3b8702ad417
Eshun Calvin Williamson <kwekujur143@gmail.com>
2024-09-03 15:16:59 +0000

    react AppRouter modified

--- React Router/AppRouter.jsx
+++ React Router/AppRouter.jsx
@@ -1,10 +1,16 @@
 import {createBrowserRouter} from "react-router-dom"
-import App from "../classmembers/src/App";
-import HomePage from "../classmembers/src/Pages/HomePage";
-import AboutPage from "../classmembers/src/Pages/AboutPage";
-import ContactPage from "../classmembers/src/Pages/ContactPage";
-import TeamPage from "../classmembers/src/Pages/TeamPage";
-import JoinPage from "../classmembers/src/Pages/JoinPage";
+// import App from "../classmembers/src/App";
+import App from "../src/App";
+// import HomePage from "../classmembers/src/Pages/HomePage";
+import HomePage from "../src/Pages/HomePage";
+// import AboutPage from "../classmembers/src/Pages/AboutPage";
+import AboutPage from "../src/Pages/AboutPage";
+// import ContactPage from "../classmembers/src/Pages/ContactPage";
+import ContactPage from "../src/Pages/ContactPage";
+// import TeamPage from "../classmembers/src/Pages/TeamPage";
+import TeamPage from "../src/Pages/TeamPage";
+// import JoinPage from "../classmembers/src/Pages/JoinPage";
+import JoinPage from "../src/Pages/JoinPage";
 
 
 const AppRouter = createBrowserRouter([
